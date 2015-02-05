15196f479d6e0ee2ca44a95bd2c7f5cceac848c2
Olivier Diotte <olivier@diotte.ca>
2015-02-05 15:03:47 -0500

    timer.vhd: Remove superfluous whitespace

--- code/timer.vhd
+++ code/timer.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    11:13:42 01/20/2015 
--- Design Name: 
--- Module Name:    afficheur - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:
+-- Design Name:
+-- Module Name:
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
 
@@ -28,11 +28,11 @@ use IEEE.numeric_std.all;
 
 entity Timer is
 	port(
-		clk   : in    std_logic; --Horloge du compteur 
-		rst	  : in	  std_logic; --Signal synchrone pour remettre à zéro le compteur
-		start_timer: in    boolean; --Signal permettant de démarrer le compteur. Doit être remis à 0 pour commencer à compter de nouveau
+		clk : in std_logic; --Horloge du compteur
+		rst : in std_logic; --Signal synchrone pour remettre à zéro le compteur
+		start_timer: in boolean; --Signal permettant de démarrer le compteur. Doit être remis à 0 pour commencer à compter de nouveau
 		clk_count: in integer; -- Nombre de coup d'horloge à compter
-		timer_done  : out   boolean -- Signal avertissant la fin du compte
+		timer_done : out boolean -- Signal avertissant la fin du compte
 		);
 end Timer;
 
@@ -45,24 +45,23 @@ begin
 	begin
 
 		if rising_edge(clk) then
-			
+
 			-- Si une remise à zéro est nécessaire
 			if( rst = '1' or start_timer = false) then
-			
+
 				timer_counter := 0;
 				timer_done <= false;
-			-- Sinon, nous comptons jusqu'à ce que le maximum soit atteint	
+			-- Sinon, nous comptons jusqu'à ce que le maximum soit atteint
 			else
-				
-				if timer_counter < clk_count then 
+
+				if timer_counter < clk_count then
 					timer_counter := timer_counter + 1;
 				else
 					timer_done <= true; --Le compte est terminé
 				end if;
-				
+
 			end if;
-			
+
 		end if;
 	end process;
 end Timer;
-
